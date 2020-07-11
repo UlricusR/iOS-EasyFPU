582d73203ab574477ec5780827407d2590977312
Ulrich R <44271004+UlricusR@users.noreply.github.com>
2020-07-11 19:37:00 +0200

    Initial Commit

---
+++ EasyFPU/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  EasyFPU
+//
+//  Created by Ulrich Rüth on 11.07.20.
+//  Copyright © 2020 Ulrich Rüth. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
