6d911b05433defef7444d839dbc5ad1c4b9e8e2a
Ulrich R <44271004+UlricusR@users.noreply.github.com>
2020-08-31 23:46:05 +0200

    New: Link to rueth.info in About

--- EasyFPU/AboutView.swift
+++ EasyFPU/AboutView.swift
@@ -15,7 +15,11 @@ struct AboutView: View {
         NavigationView {
             VStack(alignment: .leading) {
                 Text("Calculates carbs, extended carbs (aka. eCarbs or Fake Carbs) and the matching absorption time for individual food or a whole meal.").padding()
-                Text("Documentation on rueth.info").padding()
+                Text("Documentation on rueth.info")
+                .padding().foregroundColor(.blue)
+                .onTapGesture {
+                    UIApplication.shared.open(URL(string: NSLocalizedString("Home-Link", comment: ""))!)
+                }
                 Spacer()
             }
             .navigationBarTitle(Text("About this app"))
