094856f16610f99d31b3e0cc6bba8b030da48cf8
ulricusr <ulrich.rueth@gmail.com>
2023-08-17 12:10:45 +0200

    Simplified data migration
    Migration to data model 2 will not include any ComposedFoodItems or Ingredients

---
+++ EasyFPU/Migration/ComposedFoodItem1toComposedFoodItem2MigrationPolicy.swift
@@ -0,0 +1,19 @@
+//
+//  ComposedFoodItem1toComposedFoodItem2MigrationPolicy.swift
+//  EasyFPU
+//
+//  Created by Ulrich Rüth on 17/08/2023.
+//  Copyright © 2023 Ulrich Rüth. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+final class ComposedFoodItem1ToComposedFoodItem2MigrationPolicy: NSEntityMigrationPolicy {
+    /**
+     No ComposedFoodItem is created in the destination model, i.e., there will be no ComposedFoodItems
+     */
+    override func createDestinationInstances(forSource sourceComposedFoodItem: NSManagedObject, in mapping: NSEntityMapping, manager: NSMigrationManager) throws {
+        // Do nothing on purpose
+    }
+}
