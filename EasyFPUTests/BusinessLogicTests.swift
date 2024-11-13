742633c3bf63a806100513a97793fa63ddcb8879
Ulrich R <44271004+UlricusR@users.noreply.github.com>
2024-11-13 18:55:58 +0100

    Moved views into group

---
+++ EasyFPUTests/BusinessLogicTests.swift
@@ -0,0 +1,17 @@
+//
+//  BusinessLogicTests.swift
+//  EasyFPUTests
+//
+//  Created by Ulrich Rüth on 13/11/2024.
+//  Copyright © 2024 Ulrich Rüth. All rights reserved.
+//
+
+import Testing
+
+struct BusinessLogicTests {
+
+    @Test func <#test function name#>() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
